0ecc331ce3a33b96f57cbf53f819640a994ba43c
Aundra Miller <miller.aundra@gmail.com>
2016-03-25 14:49:54 -0700

    incorporate pipe into tracker home page results

--- app/tracker-home.component.ts
+++ app/tracker-home.component.ts
@@ -3,16 +3,23 @@ import { FoodDisplayComponent } from './food-display.component';
 import { FoodDetailsComponent } from './food-details.component';
 import { Food } from './food.model';
 import { AddFoodComponent } from './add-food.component';
+import { HealthyFoodPipe } from './food.pipe';
 
 @Component ({
   selector: 'tracker-home',
   inputs: ['calorieCap', 'foods'],
   directives: [FoodDisplayComponent, FoodDetailsComponent, AddFoodComponent],
+  pipes: [HealthyFoodPipe],
   template: `
     <div class="row">
       <div class="col-sm-6">
+        <select (change)="changeHealthFilter($event.target.value)">
+          <option value="healthy">Healthy</option>
+          <option value="unhealthy">Unhealthy</option>
+          <option value="no-filter" selected="selected">No Filter</option>
+        </select>
         <food-display
-        *ngFor="#food of foods"
+        *ngFor="#food of foods | healthy:healthFilter"
         [singleFood]="food"
         (foodToView)="viewSelectedFood($event)"
         >
@@ -42,14 +49,15 @@ import { AddFoodComponent } from './add-food.component';
 })
 
 export class TrackerHomeComponent {
-  //var foods not setting to input value foods
   public foods: Food[];
   public selectedFood: Food;
   public showAddFoodField: boolean;
+  public healthFilter: string;
 
   constructor() {
     this.selectedFood = undefined;
     this.showAddFoodField = false;
+    this.healthFilter = "no-filter";
   }
 
   viewSelectedFood(clickedFood: Food) {
@@ -68,6 +76,11 @@ export class TrackerHomeComponent {
 
   addNewFood(newFoodArray: any[]) {
     this.foods.push(new Food(newFoodArray[0], newFoodArray[1], newFoodArray[2], this.foods.length));
+  }
+
+  changeHealthFilter(newHealthFilter) {
+    this.healthFilter = newHealthFilter;
+    console.log(this.healthFilter);
   }
 
 
