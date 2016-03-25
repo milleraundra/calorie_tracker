1d0f9a441ba6cddbabf48c6f2bd8ae9634b6d7fd
Aundra Miller <miller.aundra@gmail.com>
2016-03-25 09:13:31 -0700

    add bootstrap structure to home page

--- app/tracker-home.component.ts
+++ app/tracker-home.component.ts
@@ -2,12 +2,37 @@ import { Component } from 'angular2/core';
 
 @Component ({
   selector: 'tracker-home',
-  inputs: ['user', 'calorieGoal'],
+  inputs: ['user', 'calorieCap'],
   template: `
-    <h3>Hello, World!</h3>
+    <div class="row">
+      <div class="col-sm-6">
+        <p>Lorem ipsum dolor si amet</p>
+        <p>Lorem ipsum dolor si amet</p>
+        <p>Lorem ipsum dolor si amet</p>
+        <p>Lorem ipsum dolor si amet</p>
+        <p>Lorem ipsum dolor si amet</p>
+        <p>Lorem ipsum dolor si amet</p>
+        <p>Lorem ipsum dolor si amet</p>
+        <p>Lorem ipsum dolor si amet</p>
+        <p>Lorem ipsum dolor si amet</p>
+        <p>Lorem ipsum dolor si amet</p>
+      </div>
+
+      <div class="col-sm-6">
+      <h3>{{ user}}</h3>
+      <h4>{{ calorieCap }}</h4>
+      </div>
+    </div>
+
   `
 })
 
 export class TrackerHomeComponent {
+  public user: String;
+  public calorieCap: Number;
+
+  constructor() {
+    console.log(this.user);
+  }
 
 }
--- questions.md
+++ questions.md
@@ -2,3 +2,4 @@
 
 1. Is the 'bootstrap' listed in the main.ts file the front-end bootstrap framework we've been using?
 2. ^If so, how would I go about using a different framework, like materialize?
+3. How do I save inputs from parent or child components as properties of the current class? Parent -> input -> Child(I want to save that as a property).
