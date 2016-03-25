34a528c67c0e20e6fa4e00c4527101f14c9b2564
Aundra Miller <miller.aundra@gmail.com>
2016-03-25 08:53:38 -0700

    create tracker home-page component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component } from 'angular2/core';
 import { SignInComponent } from './sign-in.component';
+import { TrackerHomeComponent } from './tracker-home.component';
 
 @Component({
   selector: 'my-app',
-  directives: [SignInComponent],
+  directives: [SignInComponent, TrackerHomeComponent],
   template: `
     <div class="container">
       <div class="page-header">
         <h1>Calorie Tracker</h1>
       </div>
-      <sign-in *ngIf="!userExists"(userSignIn)="runSignIn($event)"></sign-in>
+      <sign-in
+        *ngIf="!userExists"
+        (userSignIn)="runSignIn($event)">
+      </sign-in>
+      <tracker-home
+        *ngIf="userExists"
+        [user]="currentUser"
+        [calorieCap]="calorieGoal">
+      </tracker-home>
 
     </div>
   `
---
+++ app/tracker-home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from 'angular2/core';
+
+@Component ({
+  selector: 'tracker-home',
+  inputs: ['user', 'calorieGoal'],
+  template: `
+    <h3>Hello, World!</h3>
+  `
+})
+
+export class TrackerHomeComponent {
+
+}
