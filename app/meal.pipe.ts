3effc597f024b584b9f2e3737e0f0ebc4cd60996
Aundra Miller <miller.aundra@gmail.com>
2016-03-25 14:55:11 -0700

    create meal pipe

---
+++ app/meal.pipe.ts
@@ -0,0 +1,31 @@
+import { Pipe, PipeTransform } from 'angular2/core';
+import { Food } from './food.model';
+
+@Pipe ({
+  name: 'meal',
+  pure: false
+})
+
+export class MealPipe implements PipeTransform {
+  transform(input: Food[], args) {
+    var healthFilter = args[0];
+    if (healthFilter === "breakfast") {
+      return input.filter(function(food) {
+        return (food.meal === "breakfast");
+      })
+    } else if (healthFilter === "lunch") {
+      return input.filter(function(food) {
+        return (food.calories === "lunch");
+      })
+    } else if (healthFilter === "dinner") {
+      return input.filter(function(food) {
+        return (food.calories === "dinner");
+      })
+    } else if (healthFilter === "snacks") {
+      return input.filter(function(food) {
+        return (food.calories === "snacks");
+      })
+    }
+
+  }
+}
