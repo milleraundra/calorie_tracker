e60c1faa36667fe09655a3f522ca46ea6ce13292
Aundra Miller <miller.aundra@gmail.com>
2016-03-25 08:48:06 -0700

    create sign-in component and event emitter to parent

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,15 +3,30 @@ import { SignInComponent } from './sign-in.component';
 
 @Component({
   selector: 'my-app',
-  directives: [ SignInComponent],
+  directives: [SignInComponent],
   template: `
     <div class="container">
-      <sign-in></sign-in>
+      <div class="page-header">
+        <h1>Calorie Tracker</h1>
+      </div>
+      <sign-in *ngIf="!userExists"(userSignIn)="runSignIn($event)"></sign-in>
 
     </div>
   `
 })
 
 export class AppComponent {
+  userExists: boolean;
+  currentUser: string;
+  calorieGoal: number;
+  construct() {
+    this.userExists = false;
+  }
+
+  runSignIn(userInfo: any[]): void {
+    this.currentUser = userInfo[0];
+    this.calorieGoal = userInfo[1];
+    this.userExists = true;
+  }
 
 }
--- app/sign-in.component.ts
+++ app/sign-in.component.ts
@@ -1,12 +1,30 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
 
 @Component ({
   selector: 'sign-in',
+  outputs: ['userSignIn'],
   template: `
-    <h1>Hello, World!</h1>
+    <h1>Sign Up</h1>
+    <h2>Welcome! Please enter your name and *daily calorie goal.</h2>
+    <p>*Your daily calorie goal is the total number of calories you wish to consume in a day. Whether you're trying to lose, gain, or maintain your weight, this number will help keep you on track. Good luck!</p>
+    <input type="text" placeholder="Your Name" #userName>
+    <input type="number" placeholder="Calorie Goal" #calorieGoal>
+    <button (click)="createUser(userName, calorieGoal)">Begin!</button>
   `
 })
 
 export class SignInComponent {
+  // public userName: string;
+  // public calorieGoal: number;
+  public userSignIn: EventEmitter<any>;
+
+  constructor() {
+    this.userSignIn = new EventEmitter();
+  }
+
+  createUser(userName: HTMLInputElement, calories: HTMLInputElement) {
+    this.userSignIn.emit([userName.value, calories.value])
+  }
+
 
 }
