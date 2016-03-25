6e93fd25b6d13409548b4dc2812a76c5b3dd2d5b
Aundra Miller <miller.aundra@gmail.com>
2016-03-25 09:36:37 -0700

    add basic structure to food-display component

---
+++ app/food-display.component.ts
@@ -0,0 +1,12 @@
+import { Component } from 'angular2/core';
+
+@Component ({
+  selector: 'food-display',
+  template: `
+    <h3>Food Display! Hello World!</h3>
+  `
+})
+
+export class FoodDisplayComponent {
+  
+}
--- app/tracker-home.component.ts
+++ app/tracker-home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from 'angular2/core';
-import { FoodDisplayComponent } from 'food-display.component';
-import { Food } from 'food.model';
+import { FoodDisplayComponent } from './food-display.component';
+import { Food } from './food.model';
 
 @Component ({
   selector: 'tracker-home',
   inputs: ['user', 'calorieCap'],
-  directives: [Food, FoodDisplayComponent],
+  directives: [FoodDisplayComponent],
   template: `
     <div class="row">
       <div class="col-sm-6">
@@ -22,7 +22,7 @@ import { Food } from 'food.model';
 })
 
 export class TrackerHomeComponent {
-  public foods = Food[];
+  public foods: Food[];
   construct() {
     this.foods = [];
   }
